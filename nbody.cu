39ba19bd3486f8ce5866de7060076c63b8747f91
Alex <acroce@udel.edu>
2023-12-08 00:49:14 -0500

    moved the cuda error

--- nbody.cu
+++ nbody.cu
@@ -137,15 +137,17 @@ __host__ int main(int argc, char **argv)
 	cudaMemcpy(hVel, d_hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost);
 	clock_t t1=clock()-t0;
 
-	cudaError_t cudaError = cudaGetLastError();
-	if (cudaError != cudaSuccess) {
-			fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(cudaError));
-			return 1;
-	}
+
 
 	#ifdef DEBUG
 	printSystem(stdout);
 #endif
+
+cudaError_t cudaError = cudaGetLastError();
+if (cudaError != cudaSuccess) {
+		fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(cudaError));
+		return 1;
+}
 
 	printf("This took a total time of %f seconds\n",(double)t1/CLOCKS_PER_SEC);
 
