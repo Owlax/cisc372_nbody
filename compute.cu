33a8cb696322be3bb71e7b04d3cb5985dbc5ed50
Alex <acroce@udel.edu>
2023-12-08 17:47:28 -0500

    restructured to put allocates in compute.cu

--- compute.cu
+++ compute.cu
@@ -7,7 +7,10 @@
 //Parameters: None
 //Returns: None
 //Side Effect: Modifies the d_hPos and d_hVel arrays with the new positions and accelerations after 1 INTERVAL
-__global__ void compute(vector3* d_hPos, vector3* d_hVel, double* d_mass, vector3** accels, vector3* values){
+
+vector3* nums;
+vector3** accels;
+__global__ void parallel_compute(vector3* d_hPos, vector3* d_hVel, double* d_mass, vector3** accels, vector3* values){
 	//make an acceleration matrix which is NUMENTITIES squared in size;
 	//first compute the pairwise accelerations.  Effect is on the first argument.
 	//start parallel here: have each thread compute how two objects affect eachother and update the matrix
@@ -41,4 +44,33 @@ __global__ void compute(vector3* d_hPos, vector3* d_hVel, double* d_mass, vector
 			d_hPos[row][k] = 5;//=d_hVel[row][k]*INTERVAL;
 		}
 	}
+}
+
+void compute() {
+    vector3 *d_vel, *d_pos;
+    double *d_mass;
+
+    cudaMallocManaged((void**) &d_vel, (sizeof(vector3) * NUMENTITIES));
+    cudaMallocManaged((void**) &d_pos, (sizeof(vector3) * NUMENTITIES));
+	cudaMallocManaged((void**) &d_mass, (sizeof(double) * NUMENTITIES));
+
+    cudaMemcpy(d_vel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_pos, hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_mass, mass, sizeof(double) * NUMENTITIES, cudaMemcpyHostToDevice);
+
+    cudaMallocManaged((void**) &nums, sizeof(vector3)*NUMENTITIES*NUMENTITIES);
+    cudaMallocManaged((void**) &accels, sizeof(vector3*)*NUMENTITIES);
+
+    int blockSize = 256; 
+    int numBlocks = (NUMENTITIES + blockSize - 1) / blockSize;
+
+    PCompute<<<numBlocks, blockSize>>>(nums, accels, d_vel, d_pos, d_mass);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(hVel, d_hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDefault);
+    cudaMemcpy(hPos, d_hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDefault);
+    cudaMemcpy(mass, d_mass, sizeof(double) * NUMENTITIES, cudaMemcpyDefault);
+
+    cudaFree(accels);
+    cudaFree(nums);
 }
