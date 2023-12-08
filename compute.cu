b4cf827df69572321077f7fe8ee6956551eedb93
Alex <acroce@udel.edu>
2023-12-08 00:38:52 -0500

    well that didn't work

--- compute.cu
+++ compute.cu
@@ -12,7 +12,7 @@ __global__ void compute(vector3* d_hPos, vector3* d_hVel, double* d_mass, vector
 	//first compute the pairwise accelerations.  Effect is on the first argument.
 	//start parallel here: have each thread compute how two objects affect eachother and update the matrix
 	//something like set i and j to the two dimensions of the resulting accel matrix and have one thread for each pair
-	/*int col = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.y * blockDim.y + threadIdx.y;
     int row = blockIdx.x * blockDim.x + threadIdx.x;
 	
 	if (row < NUMENTITIES && col < NUMENTITIES) {
@@ -29,27 +29,7 @@ __global__ void compute(vector3* d_hPos, vector3* d_hVel, double* d_mass, vector
 		double magnitude=sqrt(magnitude_sq);
 		double accelmag=-1*GRAV_CONSTANT*d_mass[col]/magnitude_sq;
 		FILL_VECTOR(accels[row][col],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-	}*/
-	int col = blockIdx.y * blockDim.y + threadIdx.y;
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (row < NUMENTITIES && col < NUMENTITIES) {
-        accels[row * NUMENTITIES + col] = &values[row * NUMENTITIES + col];
-    }
-
-    if (row == col && row < NUMENTITIES && col < NUMENTITIES) {
-        FILL_VECTOR(accels[row][col], 0, 0, 0);
-    }
-    else if (row < NUMENTITIES && col < NUMENTITIES) {
-        vector3 distance;
-        for (int k = 0; k < 3; k++) {
-            distance[k] = d_hPos[row][k] - d_hPos[col][k];
-        }
-        double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
-        double magnitude = sqrt(magnitude_sq);
-        double accelmag = -GRAV_CONSTANT * d_mass[col] / magnitude_sq;
-        FILL_VECTOR(accels[row][col], accelmag * distance[0] / magnitude, accelmag * distance[1] / magnitude, accelmag * distance[2] / magnitude);
-    }
+	}
 }
 
 
