692abd506ebfb506731265279a0347512c142586
Alex <acroce@udel.edu>
2023-12-08 18:38:59 -0500

    removed

--- compute.cu
+++ compute.cu
@@ -33,7 +33,7 @@ __global__ void compute(vector3* d_hPos, vector3* d_hVel, double* d_mass, vector
 
 		vector3 accel_sum={0,0,0};
 		for (int k=0;k<3;k++){
-			accel_sum[0]+=accels[row][col][0];
+			accel_sum[k]+=accels[row][col][k];
 		}
 		//compute the new velocity based on the acceleration and time interval
 		//compute the new position based on the velocity and time interval
