00f89759a48fb334255fdac9dad73da5ed8753b5
Alex <acroce@udel.edu>
2023-12-08 16:18:15 -0500

    adding more memcpys

--- nbody.cu
+++ nbody.cu
@@ -131,6 +131,8 @@ __host__ int main(int argc, char **argv)
 		compute<<<blocks, BLOCK_SIZE>>>(d_hPos, d_hVel, d_mass, accels, values);
 		//compute2electricboogaloo<<<blocks, BLOCK_SIZE>>>(d_hPos, d_hVel, d_mass, accels, values);
 		cudaDeviceSynchronize();
+		cudaMemcpy(d_hVel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDefault);
+		cudaMemcpy(d_hPos, hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDefault);
 	}
 
     cudaMemcpy(hVel, d_hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDefault);
